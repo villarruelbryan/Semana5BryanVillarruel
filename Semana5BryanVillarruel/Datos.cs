13876a27334af39d343aa23fa46a08a46f99b1cd
Bryan Villarruel <villarruel_bryan@hotmail.com>
2022-05-11 21:52:10 -0500

    Agregar archivos de proyecto.

---
+++ Semana5BryanVillarruel/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana5BryanVillarruel
+{
+   public class Datos
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; }
+
+    }
+}
---
+++ Semana5BryanVillarruel/Get.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana5BryanVillarruel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Get : ContentPage
+    {
+        private const string Url = "http://192.168.100.26/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Semana5BryanVillarruel.Datos> _post;
+
+        public Get()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<Semana5BryanVillarruel.Datos> posts=JsonConvert.DeserializeObject<List<Semana5BryanVillarruel.Datos>>(content);
+            _post = new ObservableCollection<Semana5BryanVillarruel.Datos>(posts);
+            MyListView.ItemsSource = _post;
+        }
+    }
+}
